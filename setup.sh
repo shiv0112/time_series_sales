7363731f2c944d51c187b9b3437cd2796e5a9379
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-12-05 21:20:48 +0530

    setup

--- setup.sh
+++ setup.sh
@@ -1,21 +1,8 @@
 mkdir -p ~/.streamlit/
-echo "
-[server]
-headless = true
-port = 8000
-enableCORS = false
-
-[browser]
-
-# Internet address of the server server that the browser should connect to. Can be IP address or DNS name.
-# Default: 'localhost'
-serverAddress = '0.0.0.0'
-
-# Whether to send usage statistics to Streamlit.
-# Default: true
-gatherUsageStats = true
-
-# Port that the browser should use to connect to the server when in liveSave mode.
-# Default: whatever value is set in server.port.
-serverPort = 8000
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
